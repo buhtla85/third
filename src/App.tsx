b1a8ba70e909b69cc3a01239b8005c49b140ba79
buhtla85 <tamara.naunovic@gmail.com>
2019-12-19 22:31:37 +0100

    app component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Button from "@material-ui/core/Button";
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Button variant="contained" color="primary">Hello world</Button>
+                
+            </div>
+        )
+    }
+}
