76e2d224d2b14e4a470ab35b9fbc496f30bdda2f
buhtla85 <tamara.naunovic@gmail.com>
2020-01-06 11:16:48 +0100

    removed grid

--- src/AnimalCard.tsx
+++ src/AnimalCard.tsx
@@ -6,20 +6,16 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import { Animal } from "./myTypes";
-import Grid from '@material-ui/core/Grid';
-
 
 const useStyles = makeStyles({
     card: {
         maxWidth: 345
     },
     media: {
-        height: 0,
-        paddingTop: "56.25%"
+        height: 140,
+        //paddingTop: "56.25%"
     }, 
-    root: {
-        flexGrow: 1
-    }
+   
 });
 
 
@@ -28,20 +24,14 @@ export const AnimalCard = (props: {pet: Animal}) => {
     const classes = useStyles({});
 
     return (
-        <div className={classes.root}>
-            <Grid container spacing={3}>
-                <Grid item xs={6}>
-                    <Card className={classes.card}>
-                        <CardActionArea>
-                            <CardMedia className={classes.media} image={pet.photos[0].medium} title="Contemplative Reptile"/>
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="h2">{pet.name}</Typography>
-                                    <Typography variant="body2" color="textSecondary" component="p">{pet.description}</Typography>
-                                </CardContent>
-                        </CardActionArea>
-                    </Card> 
-                </Grid>
-            </Grid>
-        </div>
+        <Card className={classes.card}>
+            <CardActionArea>
+                <CardMedia className={classes.media} image={pet.photos[0].medium} title="Contemplative Reptile"/>
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">{pet.name}</Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">{pet.description}</Typography>
+                    </CardContent>
+            </CardActionArea>
+        </Card>   
     )
 };
