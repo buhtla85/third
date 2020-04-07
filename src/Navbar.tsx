17e12a33cb5b2352c382189b8054ce4ffeddcffd
buhtla85 <tamara.naunovic@gmail.com>
2020-04-07 15:39:17 +0200

    minor changes

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -3,20 +3,14 @@ import {createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu"
+import PetsIcon from '@material-ui/icons/Pets';
+
 
 const useStyles = makeStyles((theme: Theme) => 
     createStyles({
-        root: {
-            flexGrow: 1,
-        },
-        title: {
-            flexGrow: 1,
-        },
-        menuButton: {
+        icon: {
             marginRight: theme.spacing(2),
-        }
+          }
     })
 );
 
@@ -24,17 +18,13 @@ export const Navbar = () => {
     const classes = useStyles(createStyles);
 
     return (
-        <div className={classes.root}>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        Adopt a Pet 
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-        </div>
+        <AppBar position="relative">
+            <Toolbar>
+                <PetsIcon className={classes.icon}/>
+                <Typography variant="h6" color="inherit" noWrap>
+                    Adopt a Pet 
+                </Typography>
+            </Toolbar>
+        </AppBar>
     )
 }
