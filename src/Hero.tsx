02922e08963db93496635e8e1e2adef8e32cef69
buhtla85 <tamara.naunovic@gmail.com>
2020-04-08 15:51:38 +0200

    minor modifications

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -7,7 +7,7 @@ export const Hero = () => {
         <>
             <Container maxWidth="sm">
                 <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>Find A Pet</Typography>
-                <Typography variant="h5" align="center" color="textSecondary" paragraph>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum ea accusantium repellat sunt, dolore eum inventore in, officia temporibus reprehenderit commodi dolorum eius fugit vero, dicta natus accusamus pariatur officiis!</Typography>
+                <Typography variant="h5" align="center" color="textSecondary" paragraph>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum ea accusantium repellat sunt, dolore eum inventore in, officia temporibus reprehenderit pariatur officiis!</Typography>
             </Container>
         </>
     )
