d573b6426d720c1e38fe1ac3354468f42600dedb
buhtla85 <tamara.naunovic@gmail.com>
2020-04-09 22:14:53 +0200

    cleaned up app component and added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,52 @@
 import * as React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Container from "@material-ui/core/Container";
 import { Navbar } from "./Navbar";
+import { Hero } from "./Hero";
 import { Form } from "./Form";
-import {apiKey, apiSecret} from "../sensitive";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import Link from '@material-ui/core/Link';
+import Typography from "@material-ui/core/Typography";
 
-interface petFinderResponseToken {
-    token_type: string,
-    expires_in: number,
-    access_token: string
-}
+const Copyright = (): JSX.Element => {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+                <Link color="inherit" href="https://www.petfinder.com/" target="_blank">Powered by Petfinder</Link>{' '}
+                    {new Date().getFullYear()}
+                        {'.'}
+        </Typography>
+    );
+} 
 
-
-
-const fetchToken = async () => {
-    const hitServer = await fetch("https://api.petfinder.com/v2/oauth2/token", {
-        method: "POST",
-        body: `grant_type=client_credentials&client_id=${apiKey}&client_secret=${apiSecret}`,
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        }
-    });
-    const response: petFinderResponseToken = await hitServer.json();
-    console.log(response.access_token);
-    window.localStorage.setItem("token", response.access_token);
-}
-
-
-export class App extends React.Component {
-    state = {};
-
-    componentDidMount() {
-        if (window.localStorage.length === 0) {
-            fetchToken();
-        }
+const useStyles = makeStyles((theme: Theme) => createStyles({
+    heroContent: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(8, 0, 6)
+    },
+    footer: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(6),
     }
+}));
 
-    fetchAnimals = async () => {
-        let errMsg = "";
-        const token = window.localStorage.getItem("token")
-        const hitServer = await fetch("https://api.petfinder.com/v2/animals?type=dog&page=2", {
-            headers: {
-                "Content-Type": 'application/x-www-form-urlencoded',
-                "Authorization": `Bearer ${token}`
-            }
-        });
-        const response = await hitServer.json();
-    
-        if (hitServer.status === 401) {
-            fetchToken();
-            errMsg = "Access to server stoped. Please enter your values again in a few seconds...";
-            setTimeout(() => {
-                errMsg = ""
-            });
-        } else if (hitServer.status === 400) {
-            errMsg = "Your request contains invalid parameters. Please enter valid values.";
-            setTimeout(() => {
-                errMsg = ""
-            });
-            console.log(errMsg)
-        } else if (hitServer.status > 401 && hitServer.status < 600) {
-            errMsg = "Your request contains invalid parameters. Please enter valid values.";
-            setTimeout(() => {
-                errMsg = ""
-            });
-        } else {
-            console.log(response)
-        }
-    }
-    
-    render(){
-        return(
-            <div>
-                hello there
-                <button type="button" onClick={this.fetchAnimals} >Fetch!</button>
-            </div>
+export const App = () => {
+    const classes = useStyles(createStyles);
+        return (
+            <React.Fragment>
+                <CssBaseline />
+                <Navbar />
+                    <main>
+                        <div className={classes.heroContent}>
+                            <Hero />
+                        </div>
+                        <Form />
+                    </main>
+                    <footer className={classes.footer}>
+                        <Typography variant="h6" align="center" gutterBottom>Practice Project</Typography>
+                        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">Something here to give the footer a purpose!</Typography>
+                        <Copyright />
+                    </footer>
+            </React.Fragment>
         )
     }
-}
+
