b336eee2d19f17b26bbfef6799bf5f1d3d2fc319
buhtla85 <tamara.naunovic@gmail.com>
2020-04-09 22:15:36 +0200

    added props and functionalities

--- src/AnimalCard.tsx
+++ src/AnimalCard.tsx
@@ -1,37 +1,69 @@
 import * as React from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Link from "@material-ui/core/Link";
 import { Animal } from "./myTypes";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme: Theme) => createStyles({
     card: {
-        maxWidth: 345
+        height: "100%",
+        display: "flex",
+        flexDirection: "column"
     },
-    media: {
-        height: 140,
-        //paddingTop: "56.25%"
-    }, 
-   
-});
+    cardMedia: {
+        paddingTop: '56.25%'
+    },
+    cardContent: {
+        flexGrow: 1
+    }
+}));
 
 
-export const AnimalCard = (props: {pet: Animal}) => {
-    const { pet } = props;
-    const classes = useStyles({});
+export const AnimalCard = (props: { animal: Animal }) => {
+    const classes = useStyles(createStyles);
+    const { animal } = props;
+    const defaultString = "No data provided";
+    const defaultDescription = `Sorry, there is no description for ${animal.name}. Please make sure to check phone, email or link below.`
+    const defaultImg = "https://afmec.org/images/no-image-available-icon.jpg";
+
+    const checkForPhoto = (arr: [{small: string, medium: string, large: string, full: string;}]) => {
+        if (arr.length < 1) {
+            return defaultImg;
+        } else if (arr[0].large) {
+            return arr[0].large;
+        } else if (arr[0].medium) {
+            return arr[0].medium;
+        } else if (arr[0].small) {
+            return arr[0].small;
+        } else {
+            return arr[0].full
+        }
+    }
 
     return (
-        <Card className={classes.card}>
-            <CardActionArea>
-                <CardMedia className={classes.media} image={pet.photos[0]?.medium} title="Contemplative Reptile"/>
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="h2">{pet.name}</Typography>
-                        <Typography variant="body2" color="textSecondary" component="p" noWrap>{pet.description}</Typography>
+        <>
+            <Grid item xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                    <CardMedia className={classes.cardMedia} image={checkForPhoto(animal.photos)} title="Image Title" />
+                    <CardContent className={classes.cardContent}>
+                        <Typography gutterBottom variant="h5" component="h2">Name: {animal.name}</Typography>
+                        <Typography>{animal.description === null ? defaultDescription : animal.description}</Typography> 
                     </CardContent>
-            </CardActionArea>
-        </Card>   
+                    <CardActions>
+                        <Button size="small" color="primary">
+                            <Link color="inherit" href={animal.url ? animal.url : "https://www.petfinder.com"} target="_blank">{animal.url ? "Info link" : "No link"}</Link>
+                        </Button>
+                    </CardActions>
+                </Card>
+            </Grid>
+        </>
     )
-};
+}
+
+
